086835d2f91bc988c1de558c8a0b8c084f13b9ad
nvtrakhmawati <rakhmawati.novita@gmail.com>
2019-03-07 14:06:59 +0700

    javascript

---
+++ kamis/proxytia.js
@@ -0,0 +1,18 @@
+var nama = 'Nop';
+var peran = 'ksatria';
+
+if (peran === ''){
+    console.log ("Hallo " + nama + ", pilih peranmu untuk memulai game!" );
+} else if (peran === 'ksatria'){
+    console.log ("Selamat datang di Dunia Proxytia, " + nama);
+    console.log ("Hallo Ksatria " + nama + ", kamu dapat menyerang dengan senjatamu!");
+} else if (peran === 'tabib'){
+    console.log ("Selamat datang di Dunia Proxytia, " + nama);
+    console.log ("Hallo Tabib " + nama + ", kamu akan membantu temanmu yang terluka.");
+} else if (peran === 'penyihir'){
+    console.log ("Selamat datang di Dunia Proxytia, " + nama);
+    console.log ("Hallo Penyihir Zero " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
+} else {
+    console.log("Nama harus diisi!);
+}
+
